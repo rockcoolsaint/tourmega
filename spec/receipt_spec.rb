a847091213bc079b82bd584481794b850b6c04d7
Innocent Onyemaenu <innocent.onyemaenu@lixy.tech>
2018-12-06 13:32:44 +0100

    receipt class separated from receipt formatter

--- spec/receipt_spec.rb
+++ spec/receipt_spec.rb
@@ -1,5 +1,11 @@
-describe 'ExampleSpec' do
+require_relative '../receipt_class'
+
+describe 'generate_receipt spec' do
+  input = [[1, 'book', 12.49], [1, 'music cd', 14.99], [1, 'chocolate bar', 0.85]]
+  basket = Receipt.new
+  output = basket.generate_receipt(1, input)
+  expected_output = [[1, 'book', 12.49], [1, 'music cd', 16.49], [1, 'chocolate bar', 0.85], [1.50, 29.83]]
   it 'is true' do
-    expect(true).to be true
+    expect(output).to eq(expected_output)
   end
 end
